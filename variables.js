b7c70c5796391432bb980a69dc6f5fa1d082946a
Sambasivarao <samba.pendela@gmail.com>
2022-07-11 12:44:06 +0530

    var keyword

--- variables.js
+++ variables.js
@@ -64,6 +64,139 @@ console.log(x);     //null
 
 
 
+var arr1  = ["Angular",
+             "ReactJS",
+             "VueJS",
+             "NodeJS",
+             "MongoDB"];
+console.log( arr1[0], arr1[4] );
+//Angular MongoDB
+
+
+var arr2 = [100,200,300,400,500];
+arr2.forEach(function(element,index){
+    console.log(element,index);
+});
+/*
+    100 0
+    200 1
+    300 2
+    400 3
+    500 4
+*/
+
+var arr3 = ["Hello_1",
+            "Hello_2",
+            "Hello_3",
+            "Hello_4",
+            "Hello_5"];
+var arr4 = ["Welcome_1",
+            "Welcome_2",
+            "Welcome_3",
+            "Welcome_4",
+            "Welcome_5"];            
+arr3.forEach((element,index)=>{
+    console.log(element, arr4[index]);
+});
+/*
+    Hello_1 Welcome_1
+    Hello_2 Welcome_2
+    Hello_3 Welcome_3
+    Hello_4 Welcome_4
+    Hello_5 Welcome_5
+*/
+
+var arr5 = ["Angular","ReactJS","VueJS"];
+var arr6 = ["dot net","Java","Python"];
+var arr7 = ["Oracle","MySQL","SQLServer"];
+arr5.forEach((element,index)=>{
+    console.log(element,arr6[index],
+                arr7[index]);
+});
+//JSON
+//JSON Stands for Java Script Object Notation
+//JSON also called as JavaScript Objects
+//JSON, used to transfer the data over the Network
+//JSON is light weight
+//Objects --- {}
+//Arrays  --- []
+//Data    --- key&value pairs
+var obj = {
+    "key1" : "Hello_1",
+    "key2" : "Hello_2",
+    "key3" : "Hello_3"
+};
+console.log( obj.key1, obj.key2, obj.key3 );
+//Hello_1 Hello_2 Hello_3
+
+var obj1 = {
+    "sub_one" : "Angular",
+    "sub_two" : "dot net",
+    "sub_three" : "SQLServer"
+};
+
+//bigint
+//bigint is the datatype
+//bigint introduced in ES6
+//by using bigint we can store large numbers
+//range of bigint is ">2^53-1"
+//bigint numbers suffix with "n"
+var large = 123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789n;
+console.log(large);
+
+
+//Symbol
+//Symbol is the datatype
+//Symbol datatype introduced in ES6
+var secured = Symbol("Hello");
+console.log(secured);
+//Symbol(Hello)
+
+
+var _abc;
+//var 123abc;         //Error
+var _123abc;
+//var abc#123;        //Error
+//var var;              //Error
+
+//typeof
+//used to know the datatype
+console.log(typeof "Hello");//string
+console.log(typeof 100);//number
+console.log(typeof true);//boolean
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object
+console.log(typeof []);//object
+console.log(typeof 100n);//bigint
+console.log(typeof Symbol("Hello"));//symbol
+console.log(typeof function(){});  //function
+console.log(typeof {});    //object
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
